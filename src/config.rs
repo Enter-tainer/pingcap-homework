55e5e952ebf460a890f3daabbeb964096e6588c1
mgt <mgt@oi-wiki.org>
2021-03-03 01:06:57 +0800

    docs: add doc for code

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,10 @@
+/// The buffer size of the BufReader which read original data.
 pub const READ_BUFFER_SIZE: usize = 400_000_000; // 400M
+/// The buffer size of the BufReader which read data pieces.
 pub const SLICE_READ_BUFFER_SIZE: usize = 300_000_000; // 300M
+/// The **total** buffer size of the BufWrite which write data pieces.
 pub const WRITE_BUFFER_SIZE: usize = 500_000_000; // 500M
-pub const SLICE_SIZE: u64 = 500_000_000; // 500M
+/// The size of each file piece.
+pub const PIECE_SIZE: u64 = 500_000_000; // 500M
+/// The "k" of top "k" urls.
 pub const TOP_URL_COUNT: usize = 100;
