7fb0d496a4aee9fe1a567b80477be212dd0c8dc6
mgt <mgt@oi-wiki.org>
2021-03-02 17:53:06 +0800

    feat: finish url slice

---
+++ src/config.rs
@@ -0,0 +1,3 @@
+pub const READ_BUFFER_SIZE: usize = 500_000_000; // 500M
+pub const WRITE_BUFFER_SIZE: usize = 50_000_000; // 50M
+pub const SLICE_SIZE: u64 = 500_000_000; // 500M
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use ahash::{AHashMap, AHasher};
+use config::SLICE_SIZE;
+use rayon::prelude::*;
+use std::{
+    fs,
+    io::{prelude::*, BufWriter},
+};
+use std::{
+    fs::{read, File},
+    os::unix::prelude::MetadataExt,
+};
+use std::{hash::Hasher, io::BufReader};
+mod config;
+fn main() -> std::io::Result<()> {
+    let f = File::open("urls.1G.txt")?;
+    let meta = f.metadata()?;
+    let file_size = meta.size();
+    let slice_count: u64 = (file_size as f64 / SLICE_SIZE as f64).ceil() as u64;
+    let _err = fs::create_dir("data");
+    let mut file_handles: Vec<BufWriter<File>> = Vec::new();
+    for i in 0..slice_count {
+        let f = File::create(format!("data/url{}", i))?;
+        file_handles.push(BufWriter::with_capacity(config::WRITE_BUFFER_SIZE, f));
+    }
+    let mut reader = BufReader::with_capacity(config::READ_BUFFER_SIZE, f);
+    loop {
+        let mut buf: Vec<u8> = Vec::new();
+        let len = reader.read_until(b'\n', &mut buf)?;
+        if len == 0 {
+            break;
+        }
+        let mut hasher = AHasher::default();
+        hasher.write(&buf);
+        let hash = hasher.finish();
+        let bucket_number = hash % slice_count;
+        let writer = &mut file_handles[bucket_number as usize];
+        writer.write_all(&buf)?;
+    }
+
+    Ok(())
 }
