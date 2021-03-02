c9616cd702d356c5f212c46f17d57979c31e111a
mgt <mgt@oi-wiki.org>
2021-03-03 00:49:07 +0800

    chore: increase write buffer size

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
 pub const READ_BUFFER_SIZE: usize = 400_000_000; // 400M
 pub const SLICE_READ_BUFFER_SIZE: usize = 300_000_000; // 300M
-pub const WRITE_BUFFER_SIZE: usize = 400_000_000; // 400M
+pub const WRITE_BUFFER_SIZE: usize = 500_000_000; // 500M
 pub const SLICE_SIZE: u64 = 500_000_000; // 500M
 pub const TOP_URL_COUNT: usize = 100;
