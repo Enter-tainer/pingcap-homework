8f211f26413f70b3c7b378c3690e11f1f3757025
mgt <mgt@oi-wiki.org>
2021-03-06 23:34:15 +0800

    use rayon

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,10 @@
 /// The buffer size of the BufReader which read original data.
-pub const READ_BUFFER_SIZE: usize = 400_000_000; // 400M
+pub const READ_BUFFER_SIZE: usize = 1024 * 1024; // 1M
 /// The buffer size of the BufReader which read data pieces.
-pub const SLICE_READ_BUFFER_SIZE: usize = 300_000_000; // 300M
+pub const SLICE_READ_BUFFER_SIZE: usize = 1024 * 1024; // 1M
 /// The **total** buffer size of the BufWrite which write data pieces.
-pub const WRITE_BUFFER_SIZE: usize = 500_000_000; // 500M
+pub const WRITE_BUFFER_SIZE: usize = 10 * 1024 * 1024; // 10M
 /// The size of each file piece.
-pub const PIECE_SIZE: u64 = 500_000_000; // 500M
+pub const PIECE_SIZE: u64 = 500 * 1024 * 1024; // 500M
 /// The "k" of top "k" urls.
 pub const TOP_URL_COUNT: usize = 100;
