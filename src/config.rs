99fa0bf1d9522426bcf504d8d7c39e6812b1861f
mgt <mgt@oi-wiki.org>
2021-03-08 16:36:34 +0800

    fix: reduce cache size

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,7 @@
 /// The buffer size of the BufReader which read original data.
-pub const READ_BUFFER_SIZE: usize = 400_000_000; // 400M
+pub const READ_BUFFER_SIZE: usize = 4 * 1024 * 1024; // 4M
 /// The buffer size of the BufReader which read data pieces.
-pub const SLICE_READ_BUFFER_SIZE: usize = 300_000_000; // 300M
+pub const SLICE_READ_BUFFER_SIZE: usize = 4 * 1024 * 1024; // 4M
 /// The **total** buffer size of the BufWrite which write data pieces.
 pub const WRITE_BUFFER_SIZE: usize = 500_000_000; // 500M
 /// The size of each file piece.
